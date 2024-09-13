d782fa559e6a09262c74a657c2d575be52dc8f51
Shanyi Liow <shanyiliow1107@gmail.com>
2024-09-13 14:24:42 +0800

    chore: add comments

--- app/models/url.rb
+++ app/models/url.rb
@@ -12,7 +12,8 @@ class Url < ApplicationRecord
 
   def generate_short_url
     loop do
-      # Generate a short URL using SecureRandom with a length of 4 characters
+      # Generate a short URL using SecureRandom with a length of 4 characters (Because 64 ^ 4 has total combinations enough for this use case)
+      # Why no UUID? - UUID is too long, we need to keep the short url as short as possible
       self.short_url = SecureRandom.urlsafe_base64(4)
       # Ensure the generated short URL is unique
       break unless Url.exists?(short_url: short_url)
